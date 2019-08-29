9eeb463c120c1e9292af56eb1524de32a0f14e17
Brama Hendra Mahendra <bramahendramahendra@gmail.com>
2019-08-29 20:06:24 +0700

    first commit

---
+++ src/javaapplication23/JavaApplication23.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication23;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Brama Hendra Mahendra
+ */
+public class JavaApplication23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("masukkan inputan : ");
+        String s = input.next();
+        System.out.println("");
+        System.out.println("outputan : "+s);
+    }
+    
+}
